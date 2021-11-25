543aa8cbfefbd283bdf8631e29c46592e08b3339
Anton Bondoc <antonbondoc14@gmail.com>
2021-11-25 17:04:00 +0800

    added input-page functionality

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -3,14 +3,49 @@ import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'panel_widget.dart';
 
 class MapPage extends StatefulWidget {
-  const MapPage({Key? key}) : super(key: key);
+  MapPage({this.originData, this.destinationData});
 
+  final originData;
+  final destinationData;
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
   final panelController = PanelController();
+  String originName = "";
+  double? originLatitude;
+  double? originLongitude;
+
+  String destinationName = "";
+  double? destinationLatitude;
+  double? destinationLongitude;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Extracts the data to respective variables.
+    placeLocationData(widget.originData, widget.destinationData);
+  }
+
+  // Method used to extract the needed data from this JSON.
+  void placeLocationData(dynamic originData, dynamic destinationData) {
+    setState(() {
+      originName = originData['results'][0]['poi']['name'];
+      originLatitude = originData['results'][0]['position']['lat'];
+      originLongitude = originData['results'][0]['position']['lon'];
+
+      destinationName = destinationData['results'][0]['poi']['name'];
+      destinationLatitude = destinationData['results'][0]['position']['lat'];
+      destinationLongitude = destinationData['results'][0]['position']['lon'];
+
+      // For debugging purposes.
+      print(originName);
+      print(originLatitude);
+      print(originLongitude);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
